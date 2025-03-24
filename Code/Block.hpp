831f1b4a3c505ee387efaf48675b0eb8bb6886b8
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-03-24 12:40:12 +0100

    Update Block.hpp

--- Code/Block.hpp
+++ Code/Block.hpp
@@ -1,14 +1,14 @@
 #include "raylib.h"
 
-class Block: public Entity {
+class Block : public Entity {
 
 public:
 
 	//Rectangle block;
 	Color color;
 
-	Block(){}
-	Block(float x, float y, float width, float heigh, Color color_): Entity(x, y, width, heigh), color{color_} {}
+	Block() {}
+	Block(float x, float y, float width, float heigh, Color color_) : Entity(x, y, width, heigh), color{ color_ } {}
 
 
 };
