88759e1e3e95c90f0327fac23df2305375d630c8
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-03-27 13:11:18 +0100

    Update Goomba.hpp

--- include/Goomba.hpp
+++ include/Goomba.hpp
@@ -1,1 +1,16 @@
+#include "raylib.h"
+#include "Enemy.hpp"
 
+class Goomba : public Enemy {
+
+private:
+
+
+protected:
+
+
+public:
+
+	Goomba(){}
+	Goomba(float x, float y, float width, float heigh, int id, bool alive_) : Enemy(x, y, width, heigh, id, alive_){}
+};
