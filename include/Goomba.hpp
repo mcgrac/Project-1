421027b65e0d3763a4d0bf0dde72e52f3a75c95f
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-03-27 14:59:57 +0100

    Update Goomba.hpp

--- include/Goomba.hpp
+++ include/Goomba.hpp
@@ -1,16 +1,37 @@
 #include "raylib.h"
-#include "Enemy.hpp"
+
 
 class Goomba : public Enemy {
 
-private:
-
-
 protected:
 
+	float movementSpeed;
 
 public:
 
 	Goomba(){}
-	Goomba(float x, float y, float width, float heigh, int id, bool alive_) : Enemy(x, y, width, heigh, id, alive_){}
+	Goomba(float x, float y, float width, float heigh, int id, bool alive_ , float movementSpeed_) : Enemy(x, y, width, heigh, id, alive_), movementSpeed(movementSpeed_){}
+
+	float getMovementSpeed() {
+
+		return movementSpeed;
+	}
+
+	void moveGoomba() {
+
+		//gravity
+		hitbox.y += movementSpeed;
+	}
+
+	void colisionGoomba(Entity e) {
+
+		if (e.id == 2) { //colisiona con bloque
+
+			hitbox.y = e.hitbox.y - hitbox.height;
+		}
+		else if (e.id == 0) { // colisiona con player
+
+
+		}
+	}
 };
