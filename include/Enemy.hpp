da37ca3441e6380c86ad9fd46b174f1ce65a71a4
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-03-31 00:59:00 +0200

    Update Enemy.hpp

--- include/Enemy.hpp
+++ include/Enemy.hpp
@@ -7,15 +7,20 @@ class Enemy : public Entity {
 
 protected:
 
-	bool alive;
+
 
 public:
 
 	Enemy(){}
-	Enemy(float x, float y, float width, float heigh, int id, bool alive_):Entity(x, y, width, heigh, id), alive(alive_){}
+	Enemy(float x, float y, float width, float heigh, int id, int state):Entity(x, y, width, heigh, id, state){}
 
-	void die() {
+	//void die() {
 
+	//	alive = false;
+	//}
 
-	}
+	//bool getAlive() {
+
+	//	return alive;
+	//}
 };
