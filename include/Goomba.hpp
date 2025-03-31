2109e922172d53506ba99e5984016f2ca1ceb652
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-03-31 12:48:26 +0200

    Update Goomba.hpp

--- include/Goomba.hpp
+++ include/Goomba.hpp
@@ -6,11 +6,12 @@ class Goomba : public Enemy {
 protected:
 
 	float movementSpeed;
+	int direction;
 
 public:
 
 	Goomba(){}
-	Goomba(float x, float y, float width, float heigh, int id, int state, float movementSpeed_) : Enemy(x, y, width, heigh, id, state), movementSpeed(movementSpeed_){}
+	Goomba(float x, float y, float width, float heigh, int id, int state, float movementSpeed_, int direction_) : Enemy(x, y, width, heigh, id, state), movementSpeed(movementSpeed_), direction(direction_){}
 
 	float getMovementSpeed() {
 
@@ -19,24 +20,34 @@ public:
 
 	void moveGoomba(vector<Entity> entity) {
 
+
+
 		//gravity
 		hitbox.y += movementSpeed;
 
 		//lateral movement
-		int direction = 1; //1 right, -1 left
 
-		//if (direction == 1) {
+		printf("Direction: %d\n", direction);
 
-		//	hitbox.x += (movementSpeed - 4);
-		//}
-		//else {
+		if (direction == 1) {
 
-		//	hitbox.x -= (movementSpeed - 4);
-		//}
+			hitbox.x += (movementSpeed - 4);
+		}
+		else {
+
+			hitbox.x -= (movementSpeed - 4);
+		}
 
 		for (int i = 0; i < entity.size(); ++i) {
 
 			if (entity[i].id == 2 && CheckCollisionRecs(hitbox, entity[i].hitbox)) { //block
+
+				if (CheckCollisionPointRec(bottom, entity[i].hitbox)) {
+
+					printf("colision bottom\n");
+
+					hitbox.y = entity[i].hitbox.y - hitbox.height;
+				}
 
 				if (CheckCollisionPointRec(right, entity[i].hitbox) && direction == 1) { //right
 
@@ -48,16 +59,36 @@ public:
 					direction = 1;
 				}
 
-				
+				//if (Collidingbottom(entity[i])) {
+
+				//	hitbox.y = entity[i].hitbox.y - hitbox.height;
+				//}
+
+				/*if (CollidingAbove(entity[i])) {
+
+
+
+				}*/
 			}
 		}
+
+
 	}
 
 	void colisionsGoomba(vector<Entity> e) {
 
 		for (int i = 0; i < e.size(); ++i) {
 
-			if (CheckCollisionRecs(hitbox, e[i].hitbox)) {
+			//if (CheckCollisionRecs(hitbox, e[i].hitbox)) {
+
+			//	if (e[i].id == 2) { //block
+
+			//		hitbox.y = e[i].hitbox.y - hitbox.height;
+
+			//	}
+			//}
+
+			if (CheckCollisionPointRec(bottom, e[i].hitbox)) {
 
 				if (e[i].id == 2) { //block
 
