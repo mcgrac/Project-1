b2c4af996f2794e55b7a293c4fb59458640744ed
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-10 15:06:55 +0200

    Update GameManager.hpp

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -3,17 +3,50 @@
 class GameManager {
 private:
 
-	int titleScreen = 1;
-	int op = 0;
+	int titleScreen; // 1
+	int op; // 0
+	bool mapDrawed;
+
+	//screens
+	Texture2D backgroundTexture;
+	Texture2D Title_screenTexture;
+	Texture2D Game_overTexture;
+	Texture2D StrtTexture;
+	Texture2D Initial_screenTexture;
+	Texture2D howToPlayTexture1;
+	Texture2D howToPlayTexture2;
+	Texture2D winTexture;
+
+	//arrow
+	Texture2D arrowTexture;
 
 public:
 
-	//GameManager(){}
+	GameManager(int screen, int op_, bool drawed_):titleScreen(screen), op(op_), mapDrawed(drawed_){
+	
+		//Screens
+		backgroundTexture = LoadTexture("resources/Background.png");
+		Title_screenTexture = LoadTexture("resources/Title_screen.png");
+		Game_overTexture = LoadTexture("resources/game_over.png");
+		StrtTexture = LoadTexture("resources/strt.png");
+		Initial_screenTexture = LoadTexture("resources/Initial_screen.png");
+		howToPlayTexture1 = LoadTexture("resources/howToPlay.png");
+		howToPlayTexture2 = LoadTexture("resources/howToPlay2.png");
+		winTexture = LoadTexture("resources/tyMario.png");
 
+		
+		//arrow
+		arrowTexture = LoadTexture("resources/arrow.png");
+	}
 
-	static GameManager& GetInstance() {
-		static GameManager instance;
-		return instance;
+	void mapCreated() {
+
+		mapDrawed = true;
+	}
+
+	bool getMapCreated() {
+
+		return mapDrawed;
 	}
 
 	int GetScreen() { return titleScreen; }  // Getter público
@@ -27,9 +60,7 @@ public:
 
 	void drawArrow() {
 
-		Image arrow = LoadImage("resources/arrow.png");
-		Texture2D arrowTexture = LoadTextureFromImage(arrow);
-
+		
 		if (op == 0) {
 
 			DrawTexture(arrowTexture, 100, 257, WHITE);
@@ -65,18 +96,15 @@ public:
 		} 
 	}
 
-	void op() {
+	void opSelector() {
 
-		if (titleScreen == 2) {
+		if (IsKeyPressed(KEY_PAGE_DOWN)) {
 
-			if (IsKeyPressed(KEY_DOWN)) {
+			opDown();
+		}
+		else if (IsKeyPressed(KEY_PAGE_UP)) {
 
-				opDown();
-			}
-			else if (IsKeyPressed(KEY_UP)) {
-
-				opUp();
-			}
+			opUp();
 		}
 	}
 
@@ -90,24 +118,53 @@ public:
 
 	void nextScreen() {
 
-		if (titleScreen == 1) { titleScreen = 2; } //credits -> main menu
+		if (titleScreen == 1) { 
+			
+			titleScreen = 2; 
+			
+		
+		} //credits -> main menu
 
-		else if (titleScreen == 2 && op == 0) { titleScreen = 3; } //main menu-> world screen
+		else if (titleScreen == 2 && op == 0) { 
 
-		else if (titleScreen == 2 && op == 1) { titleScreen = 6; } //main menu -> how to play 1
+			titleScreen = 3;
+			
+		} //main menu-> world screen
 
-		else if (titleScreen == 2 && op == 2) { CloseWindow(); } //main menu -> Exit
+		else if (titleScreen == 2 && op == 1) { 
+			titleScreen = 6;
+			
+		} //main menu -> how to play 1
 
-		else if (titleScreen == 3) { titleScreen = 0; } //world screen -> play screen
+		else if (titleScreen == 2 && op == 2) {
+			CloseWindow(); 
+		} //main menu -> Exit
 
-		else if (titleScreen == 6) { titleScreen = 7; } //how to play 1 -> how to play 2
+		else if (titleScreen == 3) {
+			titleScreen = 0;
+			
+		} //world screen -> play screen
 
-		else if (titleScreen == 7) { titleScreen = 2; } //how to play 2 -> main menu
+		else if (titleScreen == 6) {
+			titleScreen = 7;
+			
+		} //how to play 1 -> how to play 2
+
+		else if (titleScreen == 7) {
+			titleScreen = 2;
+			
+		} //how to play 2 -> main menu
 
 
-		else if (titleScreen == 0) { titleScreen = 5; } //game screen -> win screen
+		else if (titleScreen == 0) {
+			titleScreen = 5;
+			
+		} //game screen -> win screen
 
-		else if (titleScreen == 5) { titleScreen = 2; } //win screen -> main menu
+		else if (titleScreen == 5) {
+			titleScreen = 2;
+			
+		} //win screen -> main menu
 
 		//else if (titleScreen == 4) { titleScreen = 1; }
 
@@ -115,6 +172,50 @@ public:
 
 	void gameOver() { titleScreen = 4; }
 
-	void drawScreen() {}
+	void drawScreen() {
+
+		if (titleScreen == 1) {
+
+			DrawTexture(Initial_screenTexture, 0, 0, WHITE);
+		}
+		else if (titleScreen == 2) {
+
+			DrawTexture(Title_screenTexture, 0, 0, WHITE);
+
+		}
+		else if (titleScreen == 3) {
+
+			DrawTexture(StrtTexture, 0, 0, WHITE);
+
+		}
+		else if (titleScreen == 4) {
+
+
+
+		}
+		else if (titleScreen == 5) {
+
+			DrawTexture(winTexture, 0, 0, WHITE);
+
+		}
+		else if (titleScreen == 6) {
+
+			DrawTexture(howToPlayTexture1, 0, 0, WHITE);
+
+		}
+		else if (titleScreen == 7) {
+
+			DrawTexture(howToPlayTexture2, 0, 0, WHITE);
+
+		}
+		else if (titleScreen == 0) {
+
+			DrawTexture(backgroundTexture, 0, 0, WHITE);
+
+		}
+	
+	
+	
+	}
 
 };
