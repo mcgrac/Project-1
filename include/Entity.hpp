f6316eb0c912809a7e0b4fc47d8dd2ff495e4f1b
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-12 01:34:39 +0200

    Update Entity.hpp

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -1,77 +1,42 @@
 
+#ifndef ENTITY_HPP
+#define ENTITY_HPP
+
 #include "raylib.h"
-#include <math.h> 
+using namespace std;
 
 class Entity {
-
 protected:
-
-	Vector2 leftBot = { hitbox.x + (hitbox.width - (hitbox.width - 3)), hitbox.y + hitbox.height };
-	Vector2 rightBot = { hitbox.x + (hitbox.width - 3), hitbox.y + hitbox.height };
+	Rectangle hitbox;
+	Vector2 top;
+	Vector2 left;
+	Vector2 right;
+	Vector2 bottom;
+	Vector2 leftBot;
+	Vector2 rightBot;
 
 public:
+	int id;     // 0 player, 1 enemy, 2 block
+	int state;  // 0 dead, 1 little mario, 2 big mario, 3 power up mario
 
-	Rectangle hitbox;
-	//float rightPoint_x, downPoint_y;
+	Entity(float x, float y, float width, float height, int id_, int state_);
+	virtual void draw() {};
 
-	//middle points of each side
-	Vector2 top = { hitbox.x + (hitbox.width / 2), hitbox.y };
-	Vector2 left = { hitbox.x, hitbox.y + (hitbox.height / 2) };
-	Vector2 right = { hitbox.x + hitbox.width, hitbox.y + (hitbox.height / 2) };
-	Vector2 bottom = { hitbox.x + (hitbox.width / 2), hitbox.y + hitbox.height };
+	void updateRects();
+	float distancePoints(Vector2 p1, Vector2 p2);
 
-	int id; //0 player, 1 enemy, 2 block
-	int state; //0 dead, 1 little mario or alive, 2 big mario, 3 power up mario
+	bool collidingRight( Entity& e);
+	bool collidingLeft( Entity& e);
+	bool collidingBottom( Entity* e);
+	bool collidingAbove( Entity* e);
 
-	Entity() {}
-	Entity(float x, float y, float width, float heigh, int id_, int state_) :
-		hitbox{ x, y, width, heigh }, id(id_), state(state_) {
-	}
-
-	//Entity(float x, float y, float width, float heigh, float rightPoint_x, float downPoint_y) :
-	//	hitbox{ x, y, width, heigh }, initialPos{ x, y }, rightUp{ rightPoint_x, y }, rightDown{ rightPoint_x ,downPoint_y }, leftDown{ x, downPoint_y } {
-	//}
-
-	//Entity(float x, float y, float width, float heigh) :
-	//	hitbox{ x, y, width, heigh } {}
-
-	void updateRects() {
-
-		top = { hitbox.x + (hitbox.width / 2), hitbox.y };
-		left = { hitbox.x, hitbox.y + (hitbox.height / 2) };
-		right = { hitbox.x + hitbox.width, hitbox.y + (hitbox.height / 2) };
-		bottom = { hitbox.x + (hitbox.width / 2), hitbox.y + hitbox.height };
-	}
-
-	float distancePoints(Vector2 p1, Vector2 p2) {
-
-		return sqrtf(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-	}
-
-	bool collidingRight(Entity e) {
-
-		return (hitbox.x < e.hitbox.x);
-	}
-
-	bool collidingLeft(Entity e) {
-
-		return (hitbox.x > e.hitbox.x);
-	}
-
-	bool Collidingbottom(Entity e) {
-
-		return (bottom.y >= e.top.y) &&
-			(bottom.x >= e.left.x) &&
-			(bottom.x <= e.right.x);
-	}
-
-	bool CollidingAbove(Entity e) {
-
-		return (top.y <= e.bottom.y) &&  // Mario está por debajo del bloque
-			(top.y > e.top.y) &&  // Mario no está por encima del bloque
-			(top.x >= e.left.x) &&  // Mario está dentro del rango horizontal izquierdo
-			(top.x <= e.right.x);  // Mario está dentro del rango horizontal derecho
-	}
-
+	// Getters (opcionalmente)
+	Rectangle getHitbox() const { return hitbox; }
+	Vector2 getTop() const { return top; }
+	Vector2 getLeft() const { return left; }
+	Vector2 getRight() const { return right; }
+	Vector2 getBottom() const { return bottom; }
 };
+
+#endif
 
