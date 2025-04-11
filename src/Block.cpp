81f1fe57e58c12587888458a567e1d61b25cc068
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-12 01:29:04 +0200

    Create Block.cpp

---
+++ src/Block.cpp
@@ -0,0 +1,32 @@
+#include "Block.hpp"
+#include <cstdio> // para printf
+
+Block::Block(float x, float y, float width, float heigh, int id, int state, int type_)
+    : Entity(x, y, width, heigh, id, state), type(type_) {
+
+    breakBlock = LoadTexture("resources/Block2.png");
+    block = LoadTexture("resources/Block3.png");
+    //goomba1 = LoadTexture("resources/Goomba1.png");
+
+    printf("Constructor Block: x = %f, y = %f\n", x, y);
+}
+
+Block::~Block() {
+
+    UnloadTexture(breakBlock);
+    UnloadTexture(block);
+    UnloadTexture(breakBlock);
+}
+
+void Block::draw() {
+    if (type == 1) {
+
+        printf("DrawBreakBlock in x: %f and y: %f\n", hitbox.x, hitbox.y);
+        DrawRectangle(hitbox.x, hitbox.y, hitbox.width, hitbox.height, WHITE);
+        printf("\n");
+    }
+    else if (type == 2) {
+        // Aquí iría el dibujo del bloque sorpresa si se activa
+        // Ej: DrawTexture(surprise, hitbox.x, hitbox.y, WHITE);
+    }
+}
