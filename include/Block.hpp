b33ee6f210cbe6e57ebb6004eb214b3181e21f95
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-12 01:33:09 +0200

    Update Block.hpp

--- include/Block.hpp
+++ include/Block.hpp
@@ -1,45 +1,21 @@
 
-#include "raylib.h"
+#pragma once
 
+#include "raylib.h"
+#include "Entity.hpp"
 
 class Block : public Entity {
 private:
-
-	bool mapCreated;
 	Texture2D breakBlock;
-	Texture surprise;
+	Texture2D block;
+	//Texture2D goomba1;
 
 protected:
-
-	int type; //1-normal block, 2-surprise block
+	int type; // 1-normal block, 2-surprise block
 
 public:
-
-
-	Block() {}
-	Block(float x, float y, float width, float heigh, int id, int state, int type_) : Entity(x, y, width, heigh, id, state), type(type_){
-
-		breakBlock = LoadTexture("resources/Block2.png"); //type 1
-		surprise = LoadTexture("resources/Block3.png"); //type surprise (2)
-	}
-
-	bool getMapCreated() {
-
-		return mapCreated;
-	}
-
-	void drawBlock() {
-
-		if (type == 1) {
-
-			//if it is a block, draw it
-			//DrawRectangleRec(entities[i].hitbox, RED);
-			DrawTexture(breakBlock, hitbox.x, hitbox.y, WHITE);
-		}
-		else if (type == 2) {
-
-			DrawTexture(surprise, hitbox.x, hitbox.y, WHITE);
-		}
-	}
-
+	Block(float x, float y, float width, float heigh, int id, int state, int type_);
+	~Block();
+	void draw() override;
 };
+
