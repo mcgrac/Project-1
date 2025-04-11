d99e88f4dfdf3c75a942ebfd5c9261e2bc375a7d
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-12 01:36:03 +0200

    Update Goomba.hpp

--- include/Goomba.hpp
+++ include/Goomba.hpp
@@ -1,116 +1,27 @@
-#include "raylib.h"
-#include <vector>
+#pragma once
+
 #include "Enemy.hpp"
+#include <vector>
 
 class Goomba : public Enemy {
 
 private:
-
-	Texture2D goomba1;
+    Texture2D goomba1;
 
 protected:
-
-	float movementSpeed;
-	int direction;
+    float movementSpeed;
+    int direction;
 
 public:
+    Goomba(float x, float y, float width, float height, int id, int state, float movementSpeed_, int direction_);
+    ~Goomba();
 
-	Goomba(){}
-	Goomba(float x, float y, float width, float heigh, int id, int state, float movementSpeed_, int direction_) : Enemy(x, y, width, heigh, id, state), movementSpeed(movementSpeed_), direction(direction_){
+    float getMovementSpeed();
 
-		goomba1 = LoadTexture("resources/Goomba1.png");
-	
-	}
+    void draw() override;
 
-	float getMovementSpeed() {
+    void moveGoomba(std::vector<Entity*> entity);
 
-		return movementSpeed;
-	}
+    void colisionsGoomba(std::vector<Entity> e);
 
-	void drawGoomba() {
-
-		DrawTexture(goomba1, hitbox.x, hitbox.y, WHITE);
-
-	}
-
-	void moveGoomba(vector<Entity> entity) {
-
-
-
-		//gravity
-		hitbox.y += movementSpeed;
-
-		//lateral movement
-
-		//printf("Direction: %d\n", direction);
-
-		if (direction == 1) {
-
-			hitbox.x += (movementSpeed - 4);
-		}
-		else {
-
-			hitbox.x -= (movementSpeed - 4);
-		}
-
-		for (int i = 0; i < entity.size(); ++i) {
-
-			if (entity[i].id == 2 && CheckCollisionRecs(hitbox, entity[i].hitbox)) { //block
-
-				if (CheckCollisionPointRec(bottom, entity[i].hitbox)) {
-
-					//printf("colision bottom\n");
-
-					hitbox.y = entity[i].hitbox.y - hitbox.height;
-				}
-
-				if (CheckCollisionPointRec(right, entity[i].hitbox) && direction == 1) { //right
-
-					direction = -1;
-				}
-				
-				if (CheckCollisionPointRec(left, entity[i].hitbox) && direction == -1) { //left
-
-					direction = 1;
-				}
-
-				//if (Collidingbottom(entity[i])) {
-
-				//	hitbox.y = entity[i].hitbox.y - hitbox.height;
-				//}
-
-				/*if (CollidingAbove(entity[i])) {
-
-
-
-				}*/
-			}
-		}
-
-
-	}
-
-	void colisionsGoomba(vector<Entity> e) {
-
-		for (int i = 0; i < e.size(); ++i) {
-
-			//if (CheckCollisionRecs(hitbox, e[i].hitbox)) {
-
-			//	if (e[i].id == 2) { //block
-
-			//		hitbox.y = e[i].hitbox.y - hitbox.height;
-
-			//	}
-			//}
-
-			if (CheckCollisionPointRec(bottom, e[i].hitbox)) {
-
-				if (e[i].id == 2) { //block
-
-					hitbox.y = e[i].hitbox.y - hitbox.height;
-
-				}
-			}
-		}
-	}
 };
