d19470959921a5b44fce54a38c7c40daaec9759d
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-12 01:36:35 +0200

    Update Player.hpp

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,228 +1,38 @@
+#pragma once
 #include "raylib.h"
 #include "Entity.hpp"
 #include <vector>
-
-using namespace std;
 
 class Player : public Entity {
 
 private:
 
-	float time = 3.0f;
+    Texture2D littleMarioR;
+    Texture2D littleMarioL;
+
+    int direction; //1 right / 0 left
+    float time;
 
 public:
-
-	//Rectangle rect;  // Rectángulo que representa al personaje
-	Vector2 speed;   // Velocidad del personaje
-	float movementSpeed; // spped of the movement
-	bool isJumping;  // Bandera para saber si está en el aire
-	bool onGround;
-	bool colliding;
-	bool immunity;
-	//Rectangle grid; //rectangleZonaColision
-	//Texture2D bigMario1;
-
-	//constructor
-	Player(float x, float y, float width, float heigh, int id, int state, float speedX, float speedY, float movementSpeed_)
-		: Entity(x, y, width, heigh, id, state), speed{ speedX, speedY }, movementSpeed(movementSpeed_), isJumping(false), onGround(false), colliding(false), immunity(false){
-
-		/*Image bMario1Img = LoadImage("resources/bmario1.png");
-		bigMario1 = LoadTextureFromImage(bMario1Img);*/
-	}
-
-	//Player(float x, float y, float width, float heigh, float rightPoint_x, float downPoint_y, float speedX, float speedY, float movementSpeed_, int initialState)
-	//	: Entity(x, y, width, heigh, rightPoint_x, downPoint_y), speed{ speedX, speedY }, movementSpeed(movementSpeed_), isJumping(false), onGround(false), colliding(false), state(initialState) {
-	//}
-
-
-	void drawMario() {
-
-		//dubujar personaje
-		if (immunity) {
-
-			//mario just collide with the enemy
-			DrawRectangleRec(hitbox, BLUE);
-		}
-		else {
-
-			//mario is not immune
-			DrawRectangleRec(hitbox, RED);
-			//DrawTexture(bigMario1, hitbox.x, hitbox.y - hitbox.height, WHITE);
-
-			//DrawRectangleRec(mario.hitbox, RED);
-		}
-	}
-
-	void jump(float jumpForce) {
-
-		if (!isJumping) { // Solo permitir el salto si no está en el aire
-			speed.y = -jumpForce;
-			isJumping = true;
-		}
-
-	}
-
-	float getTime() { //only necessary for testing
-
-		return time;
-
-	}
-
-	void move(int direction) {
-
-		if (direction == 1) {
-
-			hitbox.x += movementSpeed;
-		}
-		else {
-
-			hitbox.x -= movementSpeed;
-		}
-
-	}
-
-	void immunityVoid(Player& mario) {
-
-		if (mario.immunity) {
-			mario.time -= GetFrameTime();
-			if (mario.time <= 0) {
-				mario.immunity = false;
-				mario.time = 3;
-			}
-		}
-	}
-
-	void colisionsPlayerEnemy(vector<Entity> enemy) {
-
-		for (int i = 0; i < enemy.size(); ++i) {
-
-			if (CheckCollisionRecs(hitbox, enemy[i].hitbox) && enemy[i].id == 1 && !immunity) { //enemies
-
-				printf("COLISION CON GOOMBA\n");
-
-				if (CheckCollisionPointRec(bottom, enemy[i].hitbox)) { //si los pies de mario colisionan con goomba
-
-					enemy[i].state = 0; //goomba muerto
-					jump(8.0f); //REVISAR SALTO
-				}
-				else { //mario pierde una vida
-
-					state--;
-
-					if (state == 0) { //si es mario pequeño, mario muere
-
-						//mario muere
-
-					}
-					else {
-
-						immunity = true;
-						//mario.immunityVoid(mario);
-					}
-				}
-
-			}
-		}
-	}
-
-	void colisionsPlayer(vector<Entity> e) {
-
-		for (int i = 0; i < e.size(); ++i) {
-
-			if (e[i].id == 2 && CheckCollisionRecs(hitbox, e[i].hitbox)) { //block
-
-				printf("colision con bloque\n");
-
-				if (Collidingbottom(e[i])) {
-
-
-					//printf("Colisión detectada BOTTOM bloque %d\n", i);
-
-					speed.y = 0;
-					hitbox.y = e[i].hitbox.y - hitbox.height; // Asegurar que Mario se quede sobre el bloque
-					isJumping = false;
-
-				}
-
-				if (CollidingAbove(e[i])) {
-
-					//printf("Colisión detectada TOP bloque %d\n", i);
-
-					hitbox.y = e[i].hitbox.y + e[i].hitbox.height; // Lo mueve justo debajo del bloque
-					speed.y = 1.0f; // Hace que caiga inmediatamente
-
-				}
-
-				if (CheckCollisionPointRec(left, e[i].hitbox)) {
-
-					//printf("Colliding left\n");
-
-					hitbox.x = e[i].hitbox.x + e[i].hitbox.width;
-				}
-
-				if (CheckCollisionPointRec(right, e[i].hitbox)) {
-
-					//printf("Colliding right\n");
-
-					hitbox.x = e[i].hitbox.x - e[i].hitbox.width;
-				}
-			}
-
-			if (CheckCollisionRecs(hitbox, e[i].hitbox) && !immunity && e[i].id == 1) { //enemies
-
-				printf("COLISION CON GOOMBA\n");
-
-				if (CheckCollisionPointRec(bottom, e[i].hitbox)) { //si los pies de mario colisionan con goomba
-
-					e[i].state = 0; //goomba muerto
-					jump(8.0f); //REVISAR SALTO
-				}
-				else { //mario pierde una vida
-
-					state--;
-
-					if (state == 0) { //si es mario pequeño, mario muere
-
-						//mario muere
-
-					}
-					else {
-
-						immunity = true;
-						//mario.immunityVoid(mario);
-					}
-				}
-				//if (CheckCollisionRecs(mario.hitbox, goomba.hitbox) && !mario.immunity) { //enemies
-
-				//	printf("COLISION CON GOOMBA\n");
-
-				//	if (CheckCollisionPointRec(mario.bottom, goomba.hitbox)) { //si los pies de mario colisionan con goomba
-
-				//		goomba.state = 0; //goomba muerto
-				//		mario.jump(8.0f); //REVISAR SALTO
-				//	}
-				//	else { //mario pierde una vida
-
-				//		mario.state--;
-
-				//		if (mario.state == 0) { //si es mario pequeño, mario muere
-
-				//			//mario muere
-
-				//		}
-				//		else {
-
-				//			mario.immunity = true;
-				//			//mario.immunityVoid(mario);
-				//		}
-				//	}
-
-				//}
-			}
-
-		}
-	}
-
-
-	
+    Vector2 speed;
+    float movementSpeed;
+    bool isJumping;
+    bool onGround;
+    bool colliding;
+    bool immunity;
+
+    Player(float x, float y, float width, float height, int id, int state, float speedX, float speedY, float movementSpeed_, int direction_);
+
+    void applyGravity(float gravity);
+    void draw() override;
+    void jump(float jumpForce);
+    float getTime();
+    void move(int direction);
+    void immunityVoid(Player& mario);
+    //void colisionsPlayerEnemy(std::vector<Entity> enemy);
+    void colisionsPlayer(std::vector<Entity*> e);
+    void changeDirection();
+
+    //getter
+    int getDir() { return direction; }
 };
