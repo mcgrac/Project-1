5dd64711691a50187038a6ef984ffc3b25744463
Orlock6400 <160230070+Orlock6400@users.noreply.github.com>
2025-04-13 19:36:52 +0200

    Create goomba animation .cpp

---
+++ Code/goomba animation .cpp
@@ -0,0 +1,77 @@
+#include "Goomba.hpp"
+#include "raylib.h"
+
+Goomba::Goomba(float x, float y, float width, float height, int id, int state, float movementSpeed_, int direction_)
+    : Enemy(x, y, width, height, id, state), movementSpeed(movementSpeed_), direction(direction_) {
+
+    goomba1 = LoadTexture("resources/Goomba1.png");
+    goomba2 = LoadTexture("resources/Goomba2.png");
+}
+
+Goomba::~Goomba() {
+
+    UnloadTexture(goomba1);
+    UnloadTexture(goomba2);
+
+}
+
+
+float Goomba::getMovementSpeed() {
+    return movementSpeed;
+}
+
+void Goomba::draw() {
+
+    if (frameCounter >= (60 / frameSpeed)) {
+        frameCounter = 0;
+        currentFrame++;
+        if (currentFrame > 1) currentFrame = 0;
+    }
+
+    if (currentFrame == 0) {
+        DrawTexture(goomba1, hitbox.x, hitbox.y, WHITE);
+ }
+    else {
+        DrawTexture(goomba2, hitbox.x, hitbox.y, WHITE);
+    }
+}
+
+
+void Goomba::moveGoomba(std::vector<Entity*> entity) {
+    // gravity
+    hitbox.y += movementSpeed;
+
+    // lateral movement
+    if (direction == 1) {
+        hitbox.x += (movementSpeed - 4);
+    }
+    else {
+        hitbox.x -= (movementSpeed - 4);
+    }
+
+    for (int i = 0; i < entity.size(); ++i) {
+        if (entity[i]->id == 2 && CheckCollisionRecs(hitbox, entity[i]->getHitbox())) { // block
+            if (CheckCollisionPointRec(bottom, entity[i]->getHitbox())) {
+                hitbox.y = entity[i]->getHitbox().y - hitbox.height;
+            }
+
+            if (CheckCollisionPointRec(right, entity[i]->getHitbox()) && direction == 1) {
+                direction = -1;
+            }
+
+            if (CheckCollisionPointRec(left, entity[i]->getHitbox()) && direction == -1) {
+                direction = 1;
+            }
+        }
+    }
+}
+
+void Goomba::colisionsGoomba(std::vector<Entity> e) {
+    for (int i = 0; i < e.size(); ++i) {
+        if (CheckCollisionPointRec(bottom, e[i].getHitbox())) {
+            if (e[i].id == 2) { // block
+                hitbox.y = e[i].getHitbox().y - hitbox.height;
+            }
+        }
+    }
+}
