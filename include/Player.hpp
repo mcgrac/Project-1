f9fe43c9273c6dd758217c46932c72208888b5a1
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-15 20:54:06 +0200

    Update Player.hpp

--- include/Player.hpp
+++ include/Player.hpp
@@ -10,10 +10,17 @@ private:
     Texture2D littleMarioR;
     Texture2D littleMarioL;
 
+    Texture2D bMarioWalkRight;
+    Texture2D bMarioWalkLeft;
+
+    Texture2D jumpMarioL;
+    Texture2D jumpMarioR;
+
+    Sound jumpSound, jumpGoombaS;
+
     int direction; //1 right / 0 left
     float time;
 
-public:
     Vector2 speed;
     float movementSpeed;
     bool isJumping;
@@ -21,18 +28,30 @@ public:
     bool colliding;
     bool immunity;
 
+    Rectangle frameRec;
+    int frameCounter;
+    int currentFrame;
+    int frameSpeed;
+    float timer;
+
+public:
+
+
     Player(float x, float y, float width, float height, int id, int state, float speedX, float speedY, float movementSpeed_, int direction_);
+    ~Player();
 
     void applyGravity(float gravity);
     void draw() override;
     void jump(float jumpForce);
     float getTime();
     void move(int direction);
-    void immunityVoid(Player& mario);
-    //void colisionsPlayerEnemy(std::vector<Entity> enemy);
-    void colisionsPlayer(std::vector<Entity*> e);
+    void immunityVoid();
+    void colisionsPlayer(std::vector<Entity*>& e);
     void changeDirection();
 
     //getter
     int getDir() { return direction; }
+    bool retImmunity() { return immunity; }
+    bool retJumping() { return isJumping; }
+    bool retColliding() { return colliding; }
 };
