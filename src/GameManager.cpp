a23e24bca37ce80a6cdec84f96ab1ba6cc86e464
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-15 20:51:31 +0200

    Update GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,5 +1,6 @@
 #include "GameManager.hpp"
 
+
 GameManager::GameManager(int screen, int op_, bool drawed_)
     : titleScreen(screen), op(op_), mapDrawed(drawed_) {
 
@@ -11,8 +12,13 @@ GameManager::GameManager(int screen, int op_, bool drawed_)
     howToPlayTexture1 = LoadTexture("resources/howToPlay.png");
     howToPlayTexture2 = LoadTexture("resources/howToPlay2.png");
     winTexture = LoadTexture("resources/tyMario.png");
-
+    looseTexture = LoadTexture("resources/game_over.png");
     arrowTexture = LoadTexture("resources/arrow.png");
+
+    //sounds
+    levelS = LoadSound("resources/audio/GroundTheme.wav");
+    gameOverS = LoadSound("resources/audio/GameOver.wav");
+    winS = LoadSound("resources/audio/LevelComplete.wav");
 }
 
 GameManager::~GameManager() {
@@ -25,6 +31,11 @@ GameManager::~GameManager() {
     UnloadTexture(howToPlayTexture2);
     UnloadTexture(winTexture);
     UnloadTexture(arrowTexture);
+    UnloadTexture(looseTexture);
+
+    UnloadSound(levelS);
+    UnloadSound(gameOverS);
+    UnloadSound(winS);
 }
 
 void GameManager::mapCreated() {
@@ -96,6 +107,10 @@ void GameManager::nextScreen() {
     else if (titleScreen == 3) {
         titleScreen = 0;
     }
+    else if (titleScreen == 4) {
+        titleScreen = 2;
+        soundPlayedOnce = false;
+    }
     else if (titleScreen == 6) {
         titleScreen = 7;
     }
@@ -114,24 +129,87 @@ void GameManager::gameOver() {
     titleScreen = 4;
 }
 
+void GameManager::win() {
+
+    titleScreen = 5;
+
+}
+
+void GameManager::playSounds() {
+
+    switch (titleScreen) {
+    case 0: //in-game screen
+        if (!IsSoundPlaying(levelS)) {
+            PlaySound(levelS);
+        }
+        break;
+    case 1: //title screen
+        //DrawTexture(Initial_screenTexture, 0, 0, WHITE);
+        break;
+    case 2: //main menu screen
+        //DrawTexture(Title_screenTexture, 0, 0, WHITE);
+        break;
+    case 3: //start level screen
+        //DrawTexture(StrtTexture, 0, 0, WHITE);
+        break;
+    case 4: //loose screen
+
+        StopSound(levelS);
+
+        if (!IsSoundPlaying(gameOverS) && !soundPlayedOnce) {
+            PlaySound(gameOverS);
+            soundPlayedOnce = true;
+        }
+
+        
+        break;
+    case 5: //win screen
+
+        StopSound(levelS);
+
+        if (!IsSoundPlaying(winS) && !soundPlayedOnce) {
+            PlaySound(winS);
+            soundPlayedOnce = true;
+        }
+
+        //DrawTexture(winTexture, 0, 0, WHITE);
+        break;
+    case 6: //how to play 1 screen
+        //DrawTexture(howToPlayTexture1, 0, 0, WHITE);
+        break;
+    case 7: //how to play 2 screen
+        //DrawTexture(howToPlayTexture2, 0, 0, WHITE);
+        break;
+    default:
+        break;
+    }
+
+}
+
 void GameManager::drawScreen() {
     switch (titleScreen) {
-    case 1:
+    case 0: //in-game screen
+        DrawTexture(backgroundTexture, 0, -32, WHITE);
+        break;
+    case 1: //title screen
         DrawTexture(Initial_screenTexture, 0, 0, WHITE);
         break;
-    case 2:
+    case 2: //main menu screen
         DrawTexture(Title_screenTexture, 0, 0, WHITE);
         break;
-    case 3:
+    case 3: //start level screen
         DrawTexture(StrtTexture, 0, 0, WHITE);
         break;
-    case 5:
+    case 4: //loose screen
+        DrawTexture(looseTexture, 0, 0, WHITE);
+        break;
+    case 5: //win screen
         DrawTexture(winTexture, 0, 0, WHITE);
         break;
-    case 6:
+    case 6: //how to play 1 screen
         DrawTexture(howToPlayTexture1, 0, 0, WHITE);
         break;
-    case 7:
+    case 7: //how to play 2 screen
         DrawTexture(howToPlayTexture2, 0, 0, WHITE);
         break;
     default:
