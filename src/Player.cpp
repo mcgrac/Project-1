1a142b6e05d7bd633f3da8433869d8fa26415fc3
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-15 20:52:03 +0200

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -13,28 +13,116 @@ Player::Player(float x, float y, float width, float height, int id, int state, f
     colliding(false),
     immunity(false),
     time(3.0f),
+    frameCounter(0),
+    currentFrame(0),
+    timer(0),
+    frameSpeed(8),
     direction(direction_){
 
+    //textures
     littleMarioR = LoadTexture("resources/mario1.png");
     littleMarioL = LoadTexture("resources/Lmario1.png");
 
+    bMarioWalkLeft = LoadTexture("resources/walking_animation0.png");
+    bMarioWalkRight = LoadTexture("resources/walking_animation1.png");
+
+    jumpMarioL = LoadTexture("resources/bMarioJumpL.png");
+    jumpMarioR = LoadTexture("resources/bMarioJumpR.png");
+
+    //sounds
+    jumpSound = LoadSound("resources/audio/jumpSmall.wav");
+    //SetSoundVolume(jumpSound, 1.0f); // Set proper volume
+    jumpGoombaS = LoadSound("resources/audio/smb_stomp.wav");
+}
+
+Player::~Player() {
+
+    UnloadTexture(littleMarioR);
+    UnloadTexture(littleMarioL);
+
+    UnloadSound(jumpSound);
+    UnloadSound(jumpGoombaS);
 }
 
 void Player::draw() {
 
+    //frameCounter++;
+
+    /*Rectangle rec = frameRec;
+
+    int frameWidth = littleMarioL.width / 4;
+    int frameHeigh = littleMarioL.height;
+
+    rec = { 0,0,(float)frameWidth, (float)frameHeigh };
+
+    float delta = GetFrameTime();
+    timer += delta;*/
+
+    //if (timer >= frameSpeed) {
+    //    timer = 0.0f;
+    //    currentFrame++;
+    //    if (currentFrame > 3) currentFrame = 0; // volvemos al primer frame
+    //    frameRec.x = (float)currentFrame * frameWidth;
+    //}
+
+
+
+    //if (onGround) {
+    //    if (frameCounter >= (60 / frameSpeed)) {
+    //        frameCounter = 0;
+    //        currentFrame++;
+    //        if (currentFrame > 3) currentFrame = 0;
+    //    }
+
+    //    frameRec.x = (float)currentFrame * frameRec.width;
+    //}
+
+    //Texture2D currentTexture;
+
+    //if (direction == 0) {
+    //    // Dirección derecha
+    //    if (onGround) {
+    //        // Mario grande caminando en el suelo, derecha
+    //        currentTexture = bMarioWalkRight;
+    //        DrawTextureRec(currentTexture, frameRec, { hitbox.x, hitbox.y-16 }, WHITE);
+    //    }
+    //    else {
+    //        // Mario grande saltando, derecha
+    //        currentTexture = jumpMarioR;
+    //        DrawTexture(currentTexture, hitbox.x, hitbox.y-16, WHITE);
+    //    }
+    //}
+    //else {
+    //    // Dirección izquierda
+    //    if (onGround) {
+    //        // Mario grande caminando en el suelo, izquierda
+    //        currentTexture = bMarioWalkLeft;
+    //        DrawTextureRec(currentTexture, frameRec, { hitbox.x, hitbox.y-16 }, WHITE);
+    //    }
+    //    else {
+    //        // Mario grande saltando, izquierda
+    //        currentTexture = jumpMarioL;
+    //        DrawTexture(currentTexture, hitbox.x, hitbox.y-16, WHITE);
+    //    }
+    //}
+
+
     if (direction == 1) {
 
         if (immunity) {
-             //to show is immune mario will "parpadear"
+
+             //to show is immune mario will blink
             if (((int)(GetTime() * 10) % 2) == 0) {
 
                 printf("----------------------parpadeando------------------");
                 DrawTexture(littleMarioR, hitbox.x, hitbox.y, WHITE);
+                //DrawTextureRec(bMarioWalkRight, frameRec, Vector2{ hitbox.x, hitbox.y }, WHITE);
             }
         }
         else {
 
             DrawTexture(littleMarioR, hitbox.x, hitbox.y, WHITE);
+            //DrawTextureRec(bMarioWalkRight, frameRec, Vector2{ hitbox.x, hitbox.y }, WHITE);
         }
 
     }
@@ -47,11 +135,13 @@ void Player::draw() {
 
                 printf("----------------------parpadeando------------------");
                 DrawTexture(littleMarioL, hitbox.x, hitbox.y, WHITE);
+                //DrawTextureRec(littleMarioL, frameRec, Vector2{ hitbox.x, hitbox.y }, WHITE);
             }
         }
         else {
 
             DrawTexture(littleMarioL, hitbox.x, hitbox.y, WHITE);
+            //DrawTextureRec(littleMarioL, frameRec, Vector2{ hitbox.x, hitbox.y }, WHITE);
         }
     }
 
@@ -59,10 +149,17 @@ void Player::draw() {
 }
 
 void Player::jump(float jumpForce) {
-    if (!isJumping) {
+    if (!isJumping || !colliding) {
         speed.y = -jumpForce;
         isJumping = true;
     }
+
+    //InitAudioDevice();
+    if (!IsSoundPlaying(jumpSound) && !IsSoundPlaying(jumpGoombaS)) {
+
+        PlaySound(jumpSound);
+    }
+
 }
 
 float Player::getTime() {
@@ -83,7 +180,7 @@ void Player::changeDirection() {
 
 void Player::applyGravity(float gravity) {
 
-    printf("Mario position: x->%f / y->%f\n", hitbox.x, hitbox.y);
+    //printf("Mario position: x->%f / y->%f\n", hitbox.x, hitbox.y);
 
     //Aplicar gravedad a MArio
     speed.y += gravity;
@@ -100,75 +197,72 @@ void Player::move(int direction) {
     }
 }
 
-void Player::immunityVoid(Player& mario) {
-    if (mario.immunity) {
-        mario.time -= GetFrameTime();
-        if (mario.time <= 0) {
-            mario.immunity = false;
-            mario.time = 3;
+void Player::immunityVoid() {
+    if (immunity) {
+        time -= GetFrameTime();
+        if (time <= 0) {
+            immunity = false;
+            time = 3;
         }
     }
 }
 
-//void Player::colisionsPlayerEnemy(std::vector<Entity> enemy) {
-//    for (int i = 0; i < enemy.size(); ++i) {
-//        if (CheckCollisionRecs(hitbox, enemy[i].hitbox) && enemy[i].id == 1 && !immunity) {
-//            printf("COLISION CON GOOMBA\n");
-//
-//            if (CheckCollisionPointRec(bottom, enemy[i].hitbox)) {
-//                enemy[i].state = 0;
-//                jump(8.0f);
-//            }
-//            else {
-//                state--;
-//                if (state == 0) {
-//                    // Mario muere
-//                }
-//                else {
-//                    immunity = true;
-//                }
-//            }
-//        }
-//    }
-//}
+void Player::colisionsPlayer(vector<Entity*>& e) {
 
-void Player::colisionsPlayer(vector<Entity*> e) {
-    for (int i = 0; i < e.size(); ++i) {
-        if (e[i]->id == 2 && CheckCollisionRecs(hitbox, e[i]->getHitbox())) {
+    for (auto it = e.begin(); it != e.end(); ) {
+        Entity* ent = *it;
+
+        // Colisiones con bloques (id == 2)
+        if (ent->id == 2 && CheckCollisionRecs(hitbox, ent->getHitbox())) {
             printf("colision con bloque\n");
 
-            if (collidingBottom(e[i])) {
+            if (collidingBottom(ent)) {
                 speed.y = 0;
-                hitbox.y = e[i]->getHitbox().y - hitbox.height;
+                hitbox.y = ent->getHitbox().y - hitbox.height;
                 isJumping = false;
+                onGround = true;
             }
 
-            if (collidingAbove(e[i])) {
-                hitbox.y = e[i]->getHitbox().y + e[i]->getHitbox().height;
+            if (collidingAbove(ent)) {
+                hitbox.y = ent->getHitbox().y + ent->getHitbox().height;
                 speed.y = 1.0f;
             }
 
-            if (CheckCollisionPointRec(left, e[i]->getHitbox())) {
-                hitbox.x = e[i]->getHitbox().x + e[i]->getHitbox().width;
+            if (CheckCollisionPointRec(left, ent->getHitbox())) {
+                hitbox.x = ent->getHitbox().x + ent->getHitbox().width;
             }
 
-            if (CheckCollisionPointRec(right, e[i]->getHitbox())) {
-                hitbox.x = e[i]->getHitbox().x - e[i]->getHitbox().width;
+            if (CheckCollisionPointRec(right, ent->getHitbox())) {
+                hitbox.x = ent->getHitbox().x - hitbox.width;
             }
+
+            colliding = true;
+
+            ++it;
+            continue;
+        }
+        else {
+
+            colliding = false;
+            onGround = false;
         }
 
-        if (CheckCollisionRecs(hitbox, e[i]->getHitbox()) && !immunity && e[i]->id == 1) {
-
-
-            if (CheckCollisionPointRec(bottom, e[i]->getHitbox())) {
-
+        // Colisiones con Goomba (id == 1)
+        if (CheckCollisionRecs(hitbox, ent->getHitbox()) && !immunity && ent->id == 1) {
+            if (CheckCollisionPointRec(bottom, ent->getHitbox()) && ent->state == 1) {
                 printf("COLLISION GOOMBA CON LOS PIES\n");
 
-                e[i]->state = 0;
+                PlaySound(jumpGoombaS);
+
+                ent->state = 0;
+
                 jump(8.0f);
+
+                delete ent;          // liberar memoria
+                it = e.erase(it);    // borrar del vector y continuar
+                continue;
             }
             else {
-
                 printf("COLISION CON GOOMBA\n");
 
                 state--;
@@ -180,5 +274,11 @@ void Player::colisionsPlayer(vector<Entity*> e) {
                 }
             }
         }
+        else {
+
+            colliding = false;
+        }
+
+        ++it;
     }
 }
