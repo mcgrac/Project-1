aa73141740cf06d5c6eb4c9a41afd56d2a52682e
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-16 23:15:46 +0200

    Update SurpriseBlock.hpp

--- include/SurpriseBlock.hpp
+++ include/SurpriseBlock.hpp
@@ -1,10 +1,29 @@
+#pragma once
 #include "raylib.h"
+#include "Block.hpp"
 
+class SurpriseBlock :public  Block{
 
-class SurpriseBlock :public Block {
+private:
+
+	Texture2D surpriseTexture;
+	Texture2D surpriseTextureEmpty;
+
+	//Texture2D mushroom;
+
+	//int typePower; // 1-mushroom, 2-flower, 3-star
+	bool powerGiven;
+protected:
+
+	//bool touched; //if its true the block won't give more powerUps
+
+	void hittedBlock() override;
 
 public:
 
+	SurpriseBlock(float x, float y, float width, float heigh, int id, int state, int type_);
 
+	void givePowerUp();
 
+	void draw() override;
 };
