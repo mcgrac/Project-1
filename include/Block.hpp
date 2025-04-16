0a8b2601a2c3c9e73a3eda6b2051b482ea9bc2e2
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-16 23:18:26 +0200

    Update Block.hpp

--- include/Block.hpp
+++ include/Block.hpp
@@ -11,12 +11,14 @@ private:
 	//Texture2D goomba1;
 
 protected:
-	int type; // 1-normal block, 2-surprise block
+	int type; // 1-normal block, 2-surprise block, 3B-breakBlock
 
 public:
 	Block(float x, float y, float width, float heigh, int id, int state, int type_);
 	~Block();
 	
-	void draw() override;
+	//void draw() override;
+
+	virtual void hittedBlock() {};
 };
 
