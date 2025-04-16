5b85cf5c35cbf56752411070f71dc1413c89be89
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-16 23:16:12 +0200

    Create PowerUp.hpp

---
+++ include/PowerUp.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include"raylib.h"
+#include"Entity.hpp"
+
+class PowerUp : public  Entity{
+
+private:
+
+	Texture2D mushroom;
+
+	int typePowerUp; //0->mushroom , 1->flower, 2->star
+
+protected:
+
+
+public:
+
+	PowerUp(float x, float y, float width, float heigh, int id, int state, int typePower_);
+
+	void draw() override;
+	
+};
+
