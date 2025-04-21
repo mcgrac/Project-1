ffa1bf4dded674a3f7757e61dc10ec674a3d1bad
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-21 18:36:39 +0200

    Update Goomba.cpp

--- src/Goomba.cpp
+++ src/Goomba.cpp
@@ -4,23 +4,27 @@
 Goomba::Goomba(float x, float y, float width, float height, int id, int state, float movementSpeed_, int direction_)
     : Enemy(x, y, width, height, id, state), movementSpeed(movementSpeed_), direction(direction_) {
 
+#pragma region TEXTURES
     goomba1 = LoadTexture("resources/textures/Goomba1.png");
     goomba2 = LoadTexture("resources/textures/Goomba2.png");
     goombaDeadT = LoadTexture("resources/textures/Goomba3.png");
+#pragma endregion
 
+#pragma region SOUNDS
     //sounds
     goombaDieS = LoadSound("resources/audio/smb_stomp.wav");
-
-    //allEntities.push_back(this);
+#pragma endregion
 
     updateRects();
 }
 
-Goomba::~Goomba() {
+Goomba::~Goomba() { //destructor
 
+#pragma region TEXTURES
     UnloadTexture(goomba1);
     UnloadTexture(goomba2);
     UnloadTexture(goombaDeadT);
+#pragma endregion
 }
 
 
@@ -28,15 +32,15 @@ float Goomba::getMovementSpeed() {
     return movementSpeed;
 }
 
-void Goomba::die(Entity*& e, vector<Entity*>& list, int index) {
-
-    delete e; 
-    // liberar memoria
-
-    //index = list.erase(index);    // borrar del vector y continuar
-    //state = 0;
-    PlaySound(goombaDieS);
-}
+//void Goomba::die(Entity*& e, vector<Entity*>& list, int index) {
+//
+//    delete e; 
+//    // liberar memoria
+//
+//    //index = list.erase(index);    // borrar del vector y continuar
+//    //state = 0;
+//    PlaySound(goombaDieS);
+//}
 
 void Goomba::draw() {
 
@@ -63,13 +67,10 @@ void Goomba::draw() {
             DrawTexture(goomba2, hitbox.x, hitbox.y, WHITE);
         }
     }
-
-
-    //DrawTexture(goomba1, hitbox.x, hitbox.y, WHITE);
 }
 
 
-void Goomba::moveGoomba(std::vector<Entity*> entity) {
+void Goomba::moveGoomba(vector<Entity*>& entity) {
     // gravity
     hitbox.y += movementSpeed;
 
@@ -81,28 +82,23 @@ void Goomba::moveGoomba(std::vector<Entity*> entity) {
         hitbox.x -= (movementSpeed - 4);
     }
     
+    collisionGoomba(entity);
+}
+
+void Goomba::collisionGoomba(vector<Entity*>& entity) {
+
     for (int i = 0; i < entity.size(); ++i) {
         if (entity[i]->getId() == 2 && CheckCollisionRecs(hitbox, entity[i]->getHitbox())) { // block
-            if (CheckCollisionPointRec(bottom, entity[i]->getHitbox())) {
+            if (CheckCollisionPointRec(bottom, entity[i]->getHitbox())) { //collision with the floor
                 hitbox.y = entity[i]->getHitbox().y - hitbox.height;
             }
 
-            if (CheckCollisionPointRec(right, entity[i]->getHitbox()) && direction == 1) {
+            if (CheckCollisionPointRec(right, entity[i]->getHitbox()) && direction == 1) { //collision right side
                 direction = -1;
             }
 
-            if (CheckCollisionPointRec(left, entity[i]->getHitbox()) && direction == -1) {
+            if (CheckCollisionPointRec(left, entity[i]->getHitbox()) && direction == -1) { //collision left side
                 direction = 1;
-            }
-        }
-    }
-}
-
-void Goomba::colisionsGoomba(std::vector<Entity> e) {
-    for (int i = 0; i < e.size(); ++i) {
-        if (CheckCollisionPointRec(bottom, e[i].getHitbox())) {
-            if (e[i].getId() == 2) { // block
-                hitbox.y = e[i].getHitbox().y - hitbox.height;
             }
         }
     }
