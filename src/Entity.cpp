64b6166419e91b2278500be5e6bbc2661aa11578
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-21 18:35:57 +0200

    Update Entity.cpp

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -3,41 +3,18 @@
 
 Entity::Entity(float x, float y, float width, float height, int id_, int state_)
     : hitbox{ x, y, width, height }, id(id_), state(state_) {
-    //printf("constructor entity x:%f y:%f\n", hitbox.x, hitbox.y);
 }
+Entity::~Entity() {};
 
 vector<Entity*> Entity::allEntities;
 
-void Entity::updateRects() {
+void Entity::updateRects() { //update the middle points of each side in the hitbox
     top = { hitbox.x + (hitbox.width / 2), hitbox.y };
     left = { hitbox.x, hitbox.y + (hitbox.height / 2) };
     right = { hitbox.x + hitbox.width, hitbox.y + (hitbox.height / 2) };
     bottom = { hitbox.x + (hitbox.width / 2), hitbox.y + hitbox.height };
 }
 
-void Entity::decreaseState() {
+void Entity::decreaseState() { //decrease when an entity is hitted
     state--;
-}
-
-bool Entity::collidingRight( Entity* e) {
-    return (hitbox.x < e->hitbox.x) &&
-        (hitbox.y <= e->hitbox.y) && (hitbox.y+hitbox.height-3 <= e->hitbox.y);
-        
-}
-
-bool Entity::collidingLeft( Entity* e) {
-    return (hitbox.x > e->hitbox.x)&&(hitbox.y <= e->hitbox.y)&&(hitbox.y + hitbox.height -3 <= e->hitbox.y);
-}
-
-bool Entity::collidingBottom( Entity* e) {
-    return (bottom.y >= e->top.y) &&
-        (bottom.x >= e->left.x) &&
-        (bottom.x <= e->right.x);
-}
-
-bool Entity::collidingAbove( Entity* e) {
-    return (top.y <= e->bottom.y) &&
-        (top.y > e->top.y) &&
-        (top.x >= e->left.x) &&
-        (top.x <= e->right.x);
 }
