4b94685c40795080fbd7673d2faf90e4b8fca19e
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-04-28 12:10:40 +0200

    Update BaseObject.hpp

--- include/BaseObject.hpp
+++ include/BaseObject.hpp
@@ -1,1 +1,19 @@
+#include"raylib.h"
+#include"Entity.hpp"
 
+class BaseObject : public Entity {
+
+private:
+
+
+protected:
+
+	int typePower; //0 -> mushroom, 1->flower, 2->star
+
+public:
+
+	BaseObject(float x, float y, float width, float height, int id, int state,int typePower_);
+
+
+	virtual void throwPower(){} //used to pop up all the power ups
+};
