cceab8280dfc04d57d270def37b8047595e81f31
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-05 12:33:24 +0200

    Update Star.hpp

--- include/Star.hpp
+++ include/Star.hpp
@@ -1,1 +1,30 @@
+#include"raylib.h"
+#include "BaseObject.hpp"
+
+class Star : public BaseObject {
+
+private:
+
+	Texture2D star;
+	Vector2 velocity{ 60.0f,0 };
+	const float terminalVelocityY = 5.0f;
+
+
+protected:
+
+public:
+
+	Star(float x, float y, float width, float height, int id, int state, int typePower_);
+	~Star();
+
+	void throwPower() override;
+
+	void update(float gravity);
+	void draw() override;
+
+	void handleCollisionX();
+	void handleCollisionY();
+
+	void handleCollision();
+};
 
