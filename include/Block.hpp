52bfa320f3b08eb296c4378b667b9d6c2f7cf31e
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-12 11:42:35 +0200

    Update Block.hpp

--- include/Block.hpp
+++ include/Block.hpp
@@ -8,6 +8,8 @@ class Block : public Entity {
 private:
 	Texture2D breakBlock;
 	Texture2D block;
+	Texture2D surpriseBlock;
+	Texture2D floorBlock;
 
 protected:
 	int type; // 1-normal block, 2-surprise block, 3B-breakBlock
