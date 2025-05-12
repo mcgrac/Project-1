3ee950131a2bb395d319cdbca4ff9b245f9604bd
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-12 11:32:24 +0200

    Update BaseObject.hpp

--- include/BaseObject.hpp
+++ include/BaseObject.hpp
@@ -10,10 +10,18 @@ protected:
 
 	int typePower; //0 -> mushroom, 1->flower, 2->star
 
+	bool emerging = true; // Al principio está saliendo del bloque
+	float emergeSpeed = 0.7f;
+	float emergedSoFar;
+	float maxEmerge = 16.0f;
+
 public:
 
 	BaseObject(float x, float y, float width, float height, int id, int state,int typePower_);
 
 
 	virtual void throwPower(){} //used to pop up all the power ups
+
+	//getter
+	int getTypePowerUp() { return typePower; }
 };
