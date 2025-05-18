64a8b62d606beefb175177d8887b3ace8d733115
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-18 11:16:22 +0200

    Update Mushroom.hpp

--- include/Mushroom.hpp
+++ include/Mushroom.hpp
@@ -1,1 +1,27 @@
+#pragma once
+#include "raylib.h"
+#include "BaseObject.hpp"
+
+class Mushroom : public BaseObject {
+private:
+    Texture2D mushroom;
+    Vector2 velocity{ 0.0f, 0.0f };
+    const float terminalVelocityY = 5.0f;
+    const float initialSpeed = 60.0f;
+
+    bool emerging = false;
+    float emergedSoFar = 0.0f;
+    const float maxEmerge = 16.0f;
+    const float emergeSpeed = 1.0f;
+
+public:
+    Mushroom(float x, float y, float width, float height, int id, int state, int typePower_);
+    ~Mushroom();
+
+    void throwPower() override;
+    void update(float gravity);
+    void draw() override;
+
+    void handleCollision();
+};
 
