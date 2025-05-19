80736f2a46120f9a7ee170e6a61a4bf44ccc7ac5
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-19 12:43:21 +0200

    Update Coin.hpp

--- include/Coin.hpp
+++ include/Coin.hpp
@@ -8,12 +8,18 @@ class Coin : public BaseObject {
 private:
 
 	Texture2D coin;
+	bool inBlock;
 
 public:
 
-	Coin(float x, float y, float width, float height, int id, int state, int typePower_);
+	Coin(float x, float y, float width, float height, int id, int state, int typePower_, bool inblock_);
 
 	~Coin();
 
 	void draw() override;
+
+	void throwPower() override;
+
+	void update();
+	void emerge();
 };
