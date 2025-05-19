05df6c60af1ba0e957fcc316e226296ec57280b3
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-19 12:45:50 +0200

    Update Coin.cpp

--- src/Coin.cpp
+++ src/Coin.cpp
@@ -1,6 +1,6 @@
 #include"Coin.hpp"
 
-Coin::Coin(float x, float y, float width, float height, int id, int state, int typePower_) : BaseObject(x, y, width, height, id, state, typePower_) {
+Coin::Coin(float x, float y, float width, float height, int id, int state, int typePower_, bool inBlock_) : BaseObject(x, y, width, height, id, state, typePower_), inBlock(inBlock_) {
 
 	coin = LoadTexture("resources/textures/coin.png");
 }
@@ -10,6 +10,40 @@ Coin::~Coin() {
 
 }
 
+void Coin::throwPower() {
+
+	emerging = true;
+	emergedSoFar = 0.0f;
+}
+
+void Coin::update() {
+
+	if (inBlock) { //only do the emerging animation if it is contained in a block
+		emerge();
+	}
+
+	if (!inBlock) { touched = true; } //if its not inside a block it touched needs to be true, otherwise player won't detect the collision
+}
+
+void Coin::emerge() {
+
+	float delta = GetFrameTime();
+
+	if (emerging && !touched) {
+
+		float move = delta * emergeSpeed * 60;
+
+		hitbox.y -= move;
+		emergedSoFar += move;
+
+		if (emergedSoFar >= maxEmerge) { // if it energe one tile it stops emerging
+			emerging = false;
+			touched = true;
+		}
+	}
+}
+
 void Coin::draw() {
 
+	DrawTexture(coin, hitbox.x, hitbox.y, WHITE);
 }
