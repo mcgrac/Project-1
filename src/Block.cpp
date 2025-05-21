f427b2c179208a62ca6f8132ae49f5254ad751e6
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-21 20:47:39 +0200

    Update Block.cpp

--- src/Block.cpp
+++ src/Block.cpp
@@ -4,16 +4,7 @@
 Block::Block(float x, float y, float width, float heigh, int id, int state, int type_)
     : Entity(x, y, width, heigh, id, state), type(type_) {
 
-    breakBlock = LoadTexture("resources/textures/Block2.png");
-    block = LoadTexture("resources/textures/Block3.png");
-
     printf("Constructor Block: x = %f, y = %f\n", x, y);
 }
 
-Block::~Block() {
-
-    UnloadTexture(breakBlock);
-    UnloadTexture(block);
-
-}
 
