853ec384f765b1e7664b16a8b258c3b01789fa34
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-21 20:48:49 +0200

    Update Entity.cpp

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 Entity::Entity(float x, float y, float width, float height, int id_, int state_)
-    : hitbox{ x, y, width, height }, id(id_), state(state_) {
+    : hitbox{ x, y, width, height }, id(id_), state(state_), toDelete(false) {
 }
 Entity::~Entity() {};
 
@@ -15,4 +15,8 @@ void Entity::updateRects() { //update the middle points of each side in the hitb
 
 void Entity::decreaseState() { //decrease when an entity is hitted
     state--;
+}
+
+void Entity::markForDelation() {
+    toDelete = true;
 }
