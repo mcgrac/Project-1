db59205c7b0b26cb2e403cd53b02c09aea8f5bac
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-21 20:41:24 +0200

    Update Block.hpp

--- include/Block.hpp
+++ include/Block.hpp
@@ -5,18 +5,13 @@
 #include "Entity.hpp"
 
 class Block : public Entity {
-private:
-	Texture2D breakBlock;
-	Texture2D block;
-	Texture2D surpriseBlock;
-	Texture2D floorBlock;
-
 protected:
-	int type; // 1-normal block, 2-surprise block, 3B-breakBlock
+	int type; // 1-normal block (invisible), 2-surprise block, 3B-breakBlock
 
 public:
+
 	Block(float x, float y, float width, float heigh, int id, int state, int type_);
-	~Block();
+	~Block() = default;
 	
 };
 
