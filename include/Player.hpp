4f16d1aea7ce02503f683d0337317dfc1b6ab476
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-25 13:28:37 +0200

    Update Player.hpp

--- include/Player.hpp
+++ include/Player.hpp
@@ -7,6 +7,8 @@
 #include "GameCamera.hpp"
 #include "Fireball.hpp"
 #include "Goomba.hpp"
+#include"Piranha.hpp"
+#include "Flag.hpp"
 
 class Player : public Entity {
 
@@ -52,6 +54,22 @@ private:
     Texture2D jumpFireRight;
 #pragma endregion
 
+#pragma region FLAG ANIMATION
+
+    //star
+    Texture2D starMarioFlag[2];
+
+    //fire
+    Texture2D fireMarioFlag[2];
+
+    //big
+    Texture2D bMarioFlag[2];
+
+    //small
+    Texture2D sMarioFlag[2];
+
+#pragma endregion
+
 #pragma endregion
 
 #pragma region SOUNDS
@@ -64,6 +82,7 @@ private:
 #pragma endregion
 
     GameCamera* camera; // pointer to camera
+    Flag* flag = nullptr; //pointer to flag for the final animation
 
     int direction; //1 right / -1 left
     float time; 
@@ -78,6 +97,10 @@ private:
     int hasPowerUp; // 0 is for nothing, 1 -> flower, 2 -> star
     int score;
     bool alive;
+    bool flagAnimation;
+
+    bool scoreAdded = false;
+    int amountScoreAdded = 0;
 
     //For spritesheet animation (NOT USED FOR NOW)
     Rectangle frameRec;
@@ -92,14 +115,14 @@ public:
     Player(float x, float y, float width, float height, int id, int state, float speedX, float speedY, float movementSpeed_, int direction_, int hasPowerUp_, GameCamera* cam_);
     ~Player();
 
-    void applyGravity(float gravity);
+    void applyGravity(float gravity, float delta);
     void draw() override;
     bool shouldDrawMario(bool immunity);
     bool shouldUseStarSprite();
     void jump(float jumpForce);
     float getTime();
-    void move(int direction, float cameraX);
-    void immunityVoid();
+    void move(int direction, float cameraX, float delta);
+    void immunityVoid(float delta);
     void colisionsPlayer(std::vector<Entity*>& e);
     void changeDirection();
     void isWalkingTrue();
@@ -109,9 +132,13 @@ public:
     void starPowerUpTimer();
     void update(vector<Entity*>& entity, float gravity) override;
     void castFireball(vector<Entity*>& entity);
-    void addScore(int scoreToAdd);
 
-    void die(float gravity);
+    void addScore(int scoreToAdd);
+    void scoreAddedFalse();
+
+    void die(float gravity, float delta);
+
+    void win(float delta);
    
 #pragma region GETTERS
     //getterS
@@ -122,5 +149,9 @@ public:
     bool retIsWalking() { return isWalking; };
     int getScorePlayer() { return score; }
     bool getAlive() { return alive; }
+    bool getFlagAnimation() { return flagAnimation; }
+    bool getState() { return state; }
+    int getScoreToAddAmount() { return amountScoreAdded; }
+    bool getScoreAdded() { return scoreAdded; }
 #pragma endregion
 };
