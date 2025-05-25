805686cdd0cf4fdc0320908ada267a05ddf4dc28
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-25 13:28:17 +0200

    Update Pipe.hpp

--- include/Pipe.hpp
+++ include/Pipe.hpp
@@ -1,1 +1,15 @@
+#pragma once
+#include"Entity.hpp"
+#include"raylib.h"
 
+class Pipe :public Entity {
+
+private:
+	Texture2D pipe;
+public:
+
+	Pipe(float x, float y, float width, float height, int id, int state);
+	~Pipe();
+
+	void draw() override;
+};
