1a9162953b6967ed4143365731a378347e25ec6e
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-05-26 18:06:19 +0200

    Delete src directory

--- src/BaseObject.cpp
+++
@@ -1,3 +0,0 @@
-#include"BaseObject.hpp"
-
-BaseObject::BaseObject(float x, float y, float width, float height, int id, int state, int typePower_) : Entity(x, y, width, height, id, state), typePower(typePower_) {};
